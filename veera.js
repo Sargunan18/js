dfea42ac3e7c00be8060abb5835e91edb232d93c
Sargunan <saragunantms3@gmail.com>
2025-12-29 22:30:21 +0530

    first commit

---
+++ veera.js
@@ -0,0 +1,3 @@
+document.getElementById("s").addEventListener("click", function () {
+    document.getElementById("love").innerHTML = "Your love percentage is " + (Math.floor(Math.random() * 100)) + "%";
+});
